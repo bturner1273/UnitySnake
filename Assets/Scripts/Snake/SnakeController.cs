54adacd2b19a8d215e53f7412c3953ff8e381c38
root <root@MSI.localdomain>
2018-10-01 18:30:39 -0400

    starting to see how the body trail is going to happen

---
+++ Assets/Scripts/Snake/SnakeController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeController : MonoBehaviour {
+    [SerializeField] private float INITIAL_MOVEMENT_INTERVAL;
+    private const float OFFSET = .5f;
+    private float movementInterval;
+    private Vector2 dir;
+    private InputHandler ih;
+    private const float MIN_INTERVAL = .05f;
+    private Vector2 lastPosition;
+    private List<GameObject> body;
+
+	// Use this for initialization
+	void Start () {
+        movementInterval = INITIAL_MOVEMENT_INTERVAL;
+        ih = GetComponent<InputHandler>();
+        body = new List<GameObject> { this.gameObject };
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameManager.IsGameActive())
+        {
+            if (movementInterval - Time.deltaTime > 0)
+            {
+                movementInterval -= Time.deltaTime;
+            }
+            else
+            {
+                lastPosition = transform.position;
+                movementInterval = INITIAL_MOVEMENT_INTERVAL;
+                if (ih.GetCurrentDirection() == MovementDirection.UP)
+                {
+                    transform.Translate(Vector2.up*OFFSET);
+                }
+                else if (ih.GetCurrentDirection() == MovementDirection.DOWN)
+                {
+                    transform.Translate(Vector2.down*OFFSET);
+                }
+                else if (ih.GetCurrentDirection() == MovementDirection.RIGHT)
+                {
+                    transform.Translate(Vector2.right*OFFSET);
+                }
+                else
+                {
+                    transform.Translate(Vector2.left*OFFSET);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (GameManager.IsGameActive())
+        {
+            if (collision.gameObject.CompareTag("SnakeBit"))
+            {
+                SpawnManager.DecrementNumSpawns();
+                SetInitMovementInterval(movementInterval - MIN_INTERVAL);
+
+                //Now that the SnakeBit objects have a
+                //translate control script, instead of destroying
+                //the collided with game object we will add it to 
+                //the body List<GameObjects> that are SnakeBits.
+                //This list of gameObjects will have their positions
+                //updated in the update function, based on current
+                //and last postitions tracked by the head and snake bits
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+
+
+    public Vector2 getLastPosition()
+    {
+        return lastPosition;
+    }
+
+    public void SetInitMovementInterval(float initMovementInterval)
+    {
+        if (initMovementInterval > MIN_INTERVAL)
+        {
+            INITIAL_MOVEMENT_INTERVAL = initMovementInterval;
+        }
+        else INITIAL_MOVEMENT_INTERVAL = MIN_INTERVAL;
+    }
+}
---
+++ Assets/Scripts/Snake/TranslateControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslateControl : MonoBehaviour {
+    Vector2 currentLocation;
+    Vector2 lastLocation;
+
+    private void Start()
+    {
+        currentLocation = new Vector2();
+        lastLocation = new Vector2();      
+    }
+
+    private void Update()
+    {
+        lastLocation = currentLocation;
+        currentLocation = transform.position;
+    }
+
+    public void SetCurrentLocation(Vector2 initCurrentLocation)
+    {
+        currentLocation = initCurrentLocation;
+    }
+
+    public Vector2 GetCurrentLocation()
+    {
+        return currentLocation;
+    }
+
+    public void SetLastLocation(Vector2 initLastLocation)
+    {
+        lastLocation = initLastLocation;
+    }
+
+    public Vector2 GetLastLocation()
+    {
+        return lastLocation;
+    }
+}
